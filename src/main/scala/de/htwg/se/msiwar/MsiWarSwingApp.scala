35a831f3622b8853a90affc70613d3bbaf6e4116
Lukas Gross <lu851gro@htwg-konstanz.de>
2017-10-12 20:50:54 +0200

    work on layout

--- src/main/scala/de/htwg/se/msiwar/MsiWarSwingApp.scala
+++ src/main/scala/de/htwg/se/msiwar/MsiWarSwingApp.scala
@@ -1,13 +1,28 @@
 package de.htwg.se.msiwar
 
 import scala.swing._
+import scala.swing.event.MouseEntered
 
-object MsiWarSwingApp extends SimpleSwingApplication{
-  def top= new MainFrame {
+object MsiWarSwingApp extends SimpleSwingApplication {
+  def top = new MainFrame {
     title = "MSI WAR"
     preferredSize = new Dimension(320, 240)
-    contents = new GridPanel(9,9){
-        contents += new Label("Test");
+
+    contents = new GridPanel(9, 9) {
+      val a = 0
+      for (a <- 0 to 81) {
+        var label = new Label("Test" + a) {
+          listenTo(mouse.moves)
+          reactions += {
+            case e: MouseEntered => onMouseEntered()
+          }
+
+          def onMouseEntered() {
+            border = new javax.swing.border.LineBorder(java.awt.Color.BLUE)
+          }
+        }
+        contents += label
+      }
     }
   }
 }
