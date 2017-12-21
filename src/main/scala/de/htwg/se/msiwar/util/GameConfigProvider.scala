ee60894f6ec2069069fa717483dd708a67b666ec
Lukas Gross <lu851gro@htwg-konstanz.de>
2017-12-21 10:08:21 +0100

    updated game graphics, finished first level

--- src/main/scala/de/htwg/se/msiwar/util/GameConfigProvider.scala
+++ src/main/scala/de/htwg/se/msiwar/util/GameConfigProvider.scala
@@ -30,15 +30,25 @@ class GameConfigProvider(configFilePath: String) {
   val wood5 = BlockObject("B", "src/main/resources/images/block_wood.png", Position(5, 4))
   val wood6 = BlockObject("B", "src/main/resources/images/block_wood.png", Position(3, 2))
   val wood7 = BlockObject("B", "src/main/resources/images/block_wood.png", Position(3, 3))
+  val wood8 = BlockObject("B", "src/main/resources/images/block_wood.png", Position(5, 0))
+  val wood9 = BlockObject("B", "src/main/resources/images/block_wood.png", Position(6, 0))
+  val wood10 = BlockObject("B", "src/main/resources/images/block_wood.png", Position(5, 8))
+  val wood11 = BlockObject("B", "src/main/resources/images/block_wood.png", Position(6, 8))
 
   val mountain1 = BlockObject("B", "src/main/resources/images/block_mountain.png", Position(7, 2))
   val mountain2 = BlockObject("B", "src/main/resources/images/block_mountain.png", Position(6, 6))
   val mountain3 = BlockObject("B", "src/main/resources/images/block_mountain.png", Position(5, 3))
   val mountain4 = BlockObject("B", "src/main/resources/images/block_mountain.png", Position(3, 1))
   val mountain5 = BlockObject("B", "src/main/resources/images/block_mountain.png", Position(6, 2))
+  val mountain6 = BlockObject("B", "src/main/resources/images/block_mountain.png", Position(0, 8))
+  val mountain7 = BlockObject("B", "src/main/resources/images/block_mountain.png", Position(1, 8))
+  val mountain8 = BlockObject("B", "src/main/resources/images/block_mountain.png", Position(0, 3))
+  val mountain9 = BlockObject("B", "src/main/resources/images/block_mountain.png", Position(0, 4))
 
   val lake1 = BlockObject("B", "src/main/resources/images/block_lake.png", Position(1, 6))
   val lake2 = BlockObject("B", "src/main/resources/images/block_lake.png", Position(8, 1))
 
-  val gameObjects = List(player1, player2, wood1, wood2, wood3, wood4, wood5, wood6, wood7, mountain1, mountain2, mountain3, mountain4, mountain5, lake1, lake2)
+  val city1 = BlockObject("B", "src/main/resources/images/block_city.png", Position(3, 5))
+
+  val gameObjects = List(player1, player2, wood1, wood2, wood3, wood4, wood5, wood6, wood7, wood8, wood9, wood10, wood11, mountain1, mountain2, mountain3, mountain4, mountain5, mountain6, mountain7, mountain8, mountain9, lake1, lake2, city1)
 }
