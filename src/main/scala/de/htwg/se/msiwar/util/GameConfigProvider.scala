8a1d89292be1512a15a25e132b8c3528d2fb5e95
Lukas Gross <lu851gro@htwg-konstanz.de>
2017-12-21 10:42:48 +0100

    updated tank graphics for 4 players and 3 tank types

--- src/main/scala/de/htwg/se/msiwar/util/GameConfigProvider.scala
+++ src/main/scala/de/htwg/se/msiwar/util/GameConfigProvider.scala
@@ -20,8 +20,8 @@ class GameConfigProvider(configFilePath: String) {
   val actions = List(moveAction, shootAction)
 
   // Setup players
-  val player1 = PlayerObject("Spieler1", "src/main/resources/images/tank_red.png", Position(1, 2), 1, 3, 3, actions)
-  val player2 = PlayerObject("Spieler2", "src/main/resources/images/tank_blue.png", Position(7, 6), 2, 3, 3, actions)
+  val player1 = PlayerObject("Spieler1", "src/main/resources/images/light_tank_red.png", Position(1, 2), 1, 3, 3, actions)
+  val player2 = PlayerObject("Spieler2", "src/main/resources/images/medium_tank_blue.png", Position(7, 6), 2, 3, 3, actions)
 
   val wood1 = BlockObject("B", "src/main/resources/images/block_wood.png", Position(0, 0))
   val wood2 = BlockObject("B", "src/main/resources/images/block_wood.png", Position(0, 1))
