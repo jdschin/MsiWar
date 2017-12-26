f91bfd885aab84f86eea9939c02756a1cb7f4046
Jan-David Schindler <jandavidschindler@gmail.com>
2017-12-26 18:27:08 +0100

    add ConfigProvider

--- app/src/main/scala/de/htwg/se/msiwar/aview/MainApp.scala
+++ app/src/main/scala/de/htwg/se/msiwar/aview/MainApp.scala
@@ -1,17 +1,26 @@
 package de.htwg.se.msiwar.aview
 
+import java.io.FileNotFoundException
 import javax.swing.SwingUtilities
 
 import de.htwg.se.msiwar.aview.tui.Tui
 import de.htwg.se.msiwar.controller.ControllerImpl
 import de.htwg.se.msiwar.model.GameModelImpl
-import de.htwg.se.msiwar.util.GameConfigProvider
+import de.htwg.se.msiwar.util.{GameConfigProvider, JSONException}
 
 object MainApp {
   def main(args: Array[String]): Unit = {
-    val configProvider = new GameConfigProvider("config.txt")
 
-    val model = GameModelImpl(configProvider.rowCount, configProvider.colCount, configProvider.gameObjects, configProvider.levelBackgroundImagePath, configProvider.actionbarBackgroundImagePath, configProvider.attackImagePath, configProvider.attackSoundPath)
+    try {
+      GameConfigProvider.loadFromFile("src/main/resources/config.json")
+    }
+    catch {
+      case e: FileNotFoundException => print(e.getMessage)
+      case e: JSONException => print(e.getMessage)
+      case e: NoSuchElementException => print(e.getMessage)
+    }
+
+    val model = GameModelImpl(GameConfigProvider.rowCount, GameConfigProvider.colCount, GameConfigProvider.gameObjects, GameConfigProvider.levelBackgroundImagePath, GameConfigProvider.actionbarBackgroundImagePath, GameConfigProvider.attackImagePath, GameConfigProvider.attackSoundPath)
     val controller = new ControllerImpl(model)
 
     new SwingApp(controller).main(args)
@@ -19,7 +28,7 @@ object MainApp {
 
     // Starts the initial game, needs to be invoked later to allow gui to be shown before
     val task = new Runnable {
-      def run() : Unit = controller.reset
+      def run(): Unit = controller.reset
     }
     SwingUtilities.invokeLater(task)
     while (tui.executeCommand(readLine())) {}
