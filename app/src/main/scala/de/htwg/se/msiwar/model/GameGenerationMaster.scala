bab6ba590c482db3d27cc3477e9d11a13b54ee07
Jan-David Schindler <jandavidschindler@gmail.com>
2018-01-08 11:51:21 +0100

    add type. Make var private

--- app/src/main/scala/de/htwg/se/msiwar/model/GameGenerationMaster.scala
+++ app/src/main/scala/de/htwg/se/msiwar/model/GameGenerationMaster.scala
@@ -6,11 +6,11 @@ import akka.routing.RoundRobinPool
 class GameGenerationMaster(numberOfWorkers: Int, numberOfMessages: Int, numberOfPlayers: Int, rowCount: Int, columnCount: Int, actions: List[Action], completion: (List[GameObject]) => Unit)
   extends Actor {
 
-  val workerRouter = context.actorOf(Props[GameGenerationWorker].withRouter(RoundRobinPool(numberOfWorkers)), name = "workerRouter")
+  private val workerRouter = context.actorOf(Props[GameGenerationWorker].withRouter(RoundRobinPool(numberOfWorkers)), name = "workerRouter")
 
-  def receive = {
+  def receive: PartialFunction[Any, Unit] = {
     case Generate =>
-      for (_  <- 0 until numberOfMessages) workerRouter ! Work(numberOfPlayers, rowCount, columnCount, actions)
+      for (_ <- 0 until numberOfMessages) workerRouter ! Work(numberOfPlayers, rowCount, columnCount, actions)
     case Result(gameObjectsOpt) =>
 
       if (gameObjectsOpt.isDefined) {
