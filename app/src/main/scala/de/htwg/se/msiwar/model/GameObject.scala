d9443b047b569ad234d8347350be8ea2a6e6079c
sammyCatlady42 <sammy_isted@t-online.de>
2019-04-03 10:59:45 +0200

    solved todo for code format

--- app/src/main/scala/de/htwg/se/msiwar/model/GameObject.scala
+++ app/src/main/scala/de/htwg/se/msiwar/model/GameObject.scala
@@ -2,8 +2,18 @@ package de.htwg.se.msiwar.model
 
 import de.htwg.se.msiwar.util.Direction.Direction
 
-abstract class GameObject(val name: String, val imagePath: String, val position: Position)
+abstract case class GameObject(name: String, imagePath: String, position: Position)
 
 case class BlockObject(override val name: String, override val imagePath: String, override val position: Position) extends GameObject(name, imagePath, position)
 
-case class PlayerObject(override val name: String, override val imagePath: String, override val position: Position, viewDirection: Direction, playerNumber: Int, wonImagePath: String, actionPoints: Int, maxActionPoints: Int, healthPoints: Int, maxHealthPoints: Int, actions: List[Action]) extends GameObject(name, imagePath, position)
+case class PlayerObject(override val name: String,
+                        override val imagePath: String,
+                        override val position: Position,
+                        viewDirection: Direction,
+                        playerNumber: Int,
+                        wonImagePath: String,
+                        actionPoints: Int,
+                        maxActionPoints: Int,
+                        healthPoints: Int,
+                        maxHealthPoints: Int,
+                        actions: List[Action]) extends GameObject(name, imagePath, position)
